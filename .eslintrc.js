0c264432c2129ec733d403f90b790c720b0cc967
Lucky Odisetti <lucky.odisetti@gmail.com>
2021-08-18 02:27:09 -0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    root: true,
+    env: {
+        es2021: true,
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        project: path.join(__dirname, './tsconfig.json'),
+        sourceType: 'module',
+    },
+    extends: [
+        '../.eslintrc.json',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn'],
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
